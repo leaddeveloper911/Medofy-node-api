28024be6eafdd4329565df10bb2649eac2275122
leaddeveloper911 <110452431+leaddeveloper911@users.noreply.github.com>
2023-01-12 18:12:17 +0530

    Update user_middleware.js

--- middleware/user_middleware.js
+++ middleware/user_middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-//JWT auth user_Middleware
+//JWT auth user
 exports.user_Middleware = (req, res, next) => {
     var Token = req.headers["authorization"]
     const bearer = Token.split(' ');
